e7e509d620274c963f40ddf751b6639072c89077
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-06-03 18:47:26 +0300

    Cake and category db models were added

---
+++ CakesProject.Infrastructure/Data/Models/Cake.cs
@@ -0,0 +1,31 @@
+﻿namespace CakesProject.Infrastructure.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Cake
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int Pieces { get; set; }
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public decimal PriceForPiece { get; set; }
+
+        public decimal PriceForCake { get; set; }
+
+        public int CakeNumber { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
