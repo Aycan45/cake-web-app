b53175e8debe746d5f7f1463e7c73a4eb723be22
Aycan45 <aycanfeyzulla@gmail.com>
2023-06-03 11:06:19 +0300

    started doing the Cake model

---
+++ CakesProjectForMom/Controllers/CakeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CakesProjectForMom.Controllers
+{
+    public class CakeController : Controller
+    {
+        public IActionResult ListCakes()
+        {
+
+
+
+            return View();
+        }
+    }
+}
